a3fbb5daea51b2f23a18024642793f62ef940eba
Kyle Glasper <ksglasper@gmail.com>
2022-09-05 15:15:56 -0500

    got background to work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,31 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, Link } from "react-router-dom";
 import Stack from "react-bootstrap/Stack";
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 
 const Header = () => {
   return (
     <>
       <header id="header">
-      <Nav variant="tabs" >
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/bootstraptest">BootStrap</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/materialuitest" eventKey="link-1" >MaterialUI</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to='/' eventKey="link-2" >
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+      
+    <Navbar bg="danger" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="#features">Features</Nav.Link>
+            <Nav.Link as={Link} to="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
 
 
-
-        <Stack direction="horizontal" gap={3}>
+        {/* <Stack direction="horizontal" gap={3}>
           <div className="bg-light border">First item</div>
           <div className="bg-light border ms-auto">Second item</div>
           <div className="bg-light border">Third item</div>
@@ -37,7 +36,7 @@ const Header = () => {
         Library
       </Breadcrumb.Item>
       <Breadcrumb.Item active>Data</Breadcrumb.Item>
-    </Breadcrumb>
+    </Breadcrumb> */}
       </header>
       <br />
       <Outlet />
