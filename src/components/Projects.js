677808129f413dea08cc9f92faa392aa4dbc0913
Kyle Glasper <ksglasper@gmail.com>
2023-09-23 04:44:29 -0500

    updating with new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,6 +25,8 @@ const Projects = () => {
   };
 
   const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
+  const [show5, setShow5] = useState(false);
 
   const handleClose3 = () => {
     setShow3(false);
@@ -32,9 +34,110 @@ const Projects = () => {
   const handleShow3 = () => {
     setShow3(true);
   };
+  const handleShow4 = () => {
+    setShow4(true);
+  };
+  const handleClose4 = () => {
+    setShow4(false);
+  };
+  const handleShow5 = () => {
+    setShow5(true);
+  };
+  const handleClose5 = () => {
+    setShow5(false);
+  };
   return (
     <>
-      <CardGroup id="project-cards">
+      <CardGroup className="project-cards">
+        <Card border="dark" bg="dark" className="single-card">
+          <Nav.Link href="https://weather-app-9f47.vercel.app/">
+            <Card.Img
+              className="site-thumbnail"
+              variant="top"
+              src="./images/weatherWithYou.PNG"
+            />
+          </Nav.Link>
+
+          <Card.Body>
+            <Card.Title className="card-title">Weather With You</Card.Title>
+            <Card.Text>
+              A weather app that users can search and favorite their favorite cities that show up on their profile. This project includes in-depth weather information at hourly, daily, and weekly intervals with easy to read charts and graphs. 
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted card-title">
+              <Button variant="test2" onClick={handleShow4}>
+                More details
+              </Button>
+              <Modal
+                show={show4}
+                onHide={handleClose4}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>Weather With You</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="project-modal">
+                  Coming Soon!
+                  {/* This project was completed by myself and 3 other classmates.
+                  The purpose was to create a complete fullstack application
+                  with plenty of functionality for both users and admin. The app
+                  allows users to remain a guest or register/login while
+                  browsing movies to buy. With 251 movie titles to choose from,
+                  users can sort through the catalog either directly using the
+                  search bar or sort by different categories including price,
+                  popularity, and title.
+                  <br />
+                  <br />
+                  The way we accessed so many titles was through another API
+                  (omdbapi) where we pulled a list of the top 250 movies,
+                  queried that database, selected the pertinent information for
+                  this project, and placed it into our own database. Users can
+                  also leave reviews for movies and check their order history
+                  once a purchase is made.
+                  <br />
+                  <br />
+                  My specific contributions include handling the outside API,
+                  displaying the movies, setting up the search bar and filter
+                  methods, navigation between pages of displayed movies, and
+                  creating the individual movie page to see full details. I was
+                  also responsible for allowing admin to edit or delete movie
+                  titles. */}
+                  <div className="icon-div">
+                    <small className="tech-used">
+                      Technologies used: PERN stack, Recharts, Vercel, React Bootstrap, JSON{" "}
+                    </small>
+                    <Nav.Link href="https://github.com/Fullstack-PostGrad-Projects/weather-app">
+                      <img
+                        className="modal-icon"
+                        src="./images/github-icon.png"
+                      ></img>
+                    </Nav.Link>
+                    <Nav.Link href="https://weather-app-9f47.vercel.app/">
+                      <img
+                        className="modal-icon"
+                        src="./images/redirect.png"
+                      ></img>
+                    </Nav.Link>
+                    {/* <Nav.Link href="https://www.youtube.com/watch?v=6cbjSG0tonU&list=PLVDIcDXQ2bNZZUw8mOHPaXIXRQbyVRdsn">
+                      <img
+                        className="modal-icon"
+                        src="./images/youtube.png"
+                      ></img>
+                    </Nav.Link> */}
+                  </div>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="dark" onClick={handleClose1}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </small>
+          </Card.Footer>
+        </Card>
         <Card border="dark" bg="dark" className="single-card">
           <Nav.Link href="https://obscure-harbor-35179.herokuapp.com/">
             <Card.Img
@@ -68,16 +171,34 @@ const Projects = () => {
                   <Modal.Title>Cinema's Top 251</Modal.Title>
                 </Modal.Header>
                 <Modal.Body className="project-modal">
-                This project was completed by myself and 3 other classmates. The purpose was to create a complete fullstack application with plenty of functionality for both users and admin. The app allows users to remain a guest or register/login while browsing movies to buy. With 251 movie titles to choose from, users can sort through the catalog either directly using the search bar or sort by different categories including price, popularity, and title. 
-                <br/>
-                <br/>
-                The way we accessed so many titles was through another API (omdbapi) where we pulled a list of the top 250 movies, queried that database, selected the pertinent information for this project, and placed it into our own database. Users can also leave reviews for movies and check their order history once a purchase is made. 
-                <br/>
-                <br/>
-                My specific contributions include handling the outside API, displaying the movies, setting up the search bar and filter methods, navigation between pages of displayed movies, and creating the individual movie page to see full details. I was also responsible for allowing admin to edit or delete movie titles.
-
+                  This project was completed by myself and 3 other classmates.
+                  The purpose was to create a complete fullstack application
+                  with plenty of functionality for both users and admin. The app
+                  allows users to remain a guest or register/login while
+                  browsing movies to buy. With 251 movie titles to choose from,
+                  users can sort through the catalog either directly using the
+                  search bar or sort by different categories including price,
+                  popularity, and title.
+                  <br />
+                  <br />
+                  The way we accessed so many titles was through another API
+                  (omdbapi) where we pulled a list of the top 250 movies,
+                  queried that database, selected the pertinent information for
+                  this project, and placed it into our own database. Users can
+                  also leave reviews for movies and check their order history
+                  once a purchase is made.
+                  <br />
+                  <br />
+                  My specific contributions include handling the outside API,
+                  displaying the movies, setting up the search bar and filter
+                  methods, navigation between pages of displayed movies, and
+                  creating the individual movie page to see full details. I was
+                  also responsible for allowing admin to edit or delete movie
+                  titles.
                   <div className="icon-div">
-                    <small className="tech-used">Technologies used: PERN stack </small>
+                    <small className="tech-used">
+                      Technologies used: PERN stack{" "}
+                    </small>
                     <Nav.Link href="https://github.com/ksglasper/GraceShopperTeamPrimer">
                       <img
                         className="modal-icon"
@@ -96,7 +217,7 @@ const Projects = () => {
                         src="./images/youtube.png"
                       ></img>
                     </Nav.Link>
-                    </div>
+                  </div>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button variant="dark" onClick={handleClose1}>
@@ -107,6 +228,96 @@ const Projects = () => {
             </small>
           </Card.Footer>
         </Card>
+        <Card border="dark" bg="dark" className="single-card">
+          <Nav.Link href="https://github.com/ksglasper/playwrightPractice">
+            <Card.Img
+              className="site-thumbnail"
+              variant="top"
+              src="./images/playwrightPractice.png"
+            />
+          </Nav.Link>
+
+          <Card.Body>
+            <Card.Title className="card-title">Netflix Playwright Unit Testing</Card.Title>
+            <Card.Text>
+              An examination opf using playwright unit testing. The project features extensive testing for Netflix's login form including: navigation, error handling, edge cases, and customizable inputs.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted card-title">
+              <Button variant="test2" onClick={handleShow5}>
+                More details
+              </Button>
+              <Modal
+                show={show5}
+                onHide={handleClose5}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>Netflix Playwirhgt Practice</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="project-modal">
+                  {/* This project was completed by myself and 3 other classmates.
+                  The purpose was to create a complete fullstack application
+                  with plenty of functionality for both users and admin. The app
+                  allows users to remain a guest or register/login while
+                  browsing movies to buy. With 251 movie titles to choose from,
+                  users can sort through the catalog either directly using the
+                  search bar or sort by different categories including price,
+                  popularity, and title.
+                  <br />
+                  <br />
+                  The way we accessed so many titles was through another API
+                  (omdbapi) where we pulled a list of the top 250 movies,
+                  queried that database, selected the pertinent information for
+                  this project, and placed it into our own database. Users can
+                  also leave reviews for movies and check their order history
+                  once a purchase is made.
+                  <br />
+                  <br />
+                  My specific contributions include handling the outside API,
+                  displaying the movies, setting up the search bar and filter
+                  methods, navigation between pages of displayed movies, and
+                  creating the individual movie page to see full details. I was
+                  also responsible for allowing admin to edit or delete movie
+                  titles. */}
+                  <div className="icon-div">
+                    <small className="tech-used">
+                      Technologies used: Javascript, Playwright, unit testing{" "}
+                    </small>
+                    <Nav.Link href="https://github.com/ksglasper/playwrightPractice">
+                      <img
+                        className="modal-icon"
+                        src="./images/github-icon.png"
+                      ></img>
+                    </Nav.Link>
+                    {/* <Nav.Link href="https://obscure-harbor-35179.herokuapp.com/">
+                      <img
+                        className="modal-icon"
+                        src="./images/redirect.png"
+                      ></img>
+                    </Nav.Link>
+                    <Nav.Link href="https://www.youtube.com/watch?v=6cbjSG0tonU&list=PLVDIcDXQ2bNZZUw8mOHPaXIXRQbyVRdsn">
+                      <img
+                        className="modal-icon"
+                        src="./images/youtube.png"
+                      ></img>
+                    </Nav.Link> */}
+                  </div>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="dark" onClick={handleClose1}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <CardGroup className="project-cards">
         <Card border="dark" bg="dark" className="single-card">
           <Nav.Link href="https://jazzy-arithmetic-3865c1.netlify.app/">
             <Card.Img
@@ -139,13 +350,26 @@ const Projects = () => {
                   <Modal.Title>Fitness Tracker</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                This was a partner project building a PERN application featuring a fitness application. The main goal was enabling users to create and search for routines and activities. As the first time doing a fullstack application there were a lot of challenges with connecting a frontend to a self-created backend. However, the benefit and knowledge gained was beyond worth. This project really established how effective it is to know the flow of information throughout the entire system when debugging and handling errors.
-                <br/>
-                <br/>
-                My specific contributions include generating the list of activities as well as the ability to create new activities. I was also in charge of attaching specific activities to routines and allowing users to search for routines based on user or featuring a specific activity.   
-
-                <div className="icon-div">
-                <small className="tech-used">Technologies used: PERN stack </small>
+                  This was a partner project building a PERN application
+                  featuring a fitness application. The main goal was enabling
+                  users to create and search for routines and activities. As the
+                  first time doing a fullstack application there were a lot of
+                  challenges with connecting a frontend to a self-created
+                  backend. However, the benefit and knowledge gained was beyond
+                  worth. This project really established how effective it is to
+                  know the flow of information throughout the entire system when
+                  debugging and handling errors.
+                  <br />
+                  <br />
+                  My specific contributions include generating the list of
+                  activities as well as the ability to create new activities. I
+                  was also in charge of attaching specific activities to
+                  routines and allowing users to search for routines based on
+                  user or featuring a specific activity.
+                  <div className="icon-div">
+                    <small className="tech-used">
+                      Technologies used: PERN stack{" "}
+                    </small>
                     <Nav.Link href="https://github.com/ksglasper/fitnessTrackerBackend">
                       <img
                         className="modal-icon"
@@ -168,7 +392,7 @@ const Projects = () => {
                         src="./images/redirect.png"
                       ></img>
                     </Nav.Link>
-                    </div>
+                  </div>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button variant="dark" onClick={handleClose2}>
@@ -210,12 +434,22 @@ const Projects = () => {
                   <Modal.Title>Tic-Tac-Toe</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                One of my first solo JavaScript projects. Focusing heavily on DOM Manipulation, this simple game of tic-tac-toe allows a user to play against another human or against a CPU. Users can input names or change them during the game and have it update accordingly. The starting playing is randomized as well the brief starting message to give users a sense of change in-between games.
-                <br/>
-                <br/>
-                When I update the project, I would like to add levels of difficulty to the CPU as well as turn this application to one of many in an arcade-like website.
-                <div className="icon-div">
-                <small className="tech-used">Technologies used: JS, DOM Manipulation, CSS, HTML5 </small>
+                  One of my first solo JavaScript projects. Focusing heavily on
+                  DOM Manipulation, this simple game of tic-tac-toe allows a
+                  user to play against another human or against a CPU. Users can
+                  input names or change them during the game and have it update
+                  accordingly. The starting playing is randomized as well the
+                  brief starting message to give users a sense of change
+                  in-between games.
+                  <br />
+                  <br />
+                  When I update the project, I would like to add levels of
+                  difficulty to the CPU as well as turn this application to one
+                  of many in an arcade-like website.
+                  <div className="icon-div">
+                    <small className="tech-used">
+                      Technologies used: JS, DOM Manipulation, CSS, HTML5{" "}
+                    </small>
                     <Nav.Link href="https://github.com/ksglasper/TicTacToeArcadeProject">
                       <img
                         className="modal-icon"
@@ -228,7 +462,7 @@ const Projects = () => {
                         src="./images/redirect.png"
                       ></img>
                     </Nav.Link>
-                    </div>
+                  </div>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button variant="dark" onClick={handleClose3}>
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -2,21 +2,29 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Figure from "react-bootstrap/Figure";
 
-
-
-
-
-const Technologies = ()=>{
-
-
-
-    return (
-        <>
-
-        <h3>Current proficiencies:</h3>
-        <Carousel>
+const Technologies = () => {
+  return (
+    <>
+      <h3>Current proficiencies:</h3>
+      <Carousel>
         <Carousel.Item>
-        <Figure className="tech-logos">
+          <Figure className="tech-logos">
+            <Figure.Image
+              className="tech-icon"
+              width={150}
+              height={150}
+              alt="171x180"
+              src="./images/react.png"
+            />
+
+            <Figure.Image
+              className="tech-icon"
+              width={150}
+              height={150}
+              alt="171x180"
+              src="./images/python.png"
+            />
+
             <Figure.Image
               className="tech-icon"
               width={150}
@@ -24,19 +32,25 @@ const Technologies = ()=>{
               alt="171x180"
               src="./images/javascript.png"
             />
-            
 
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
-              src="./images/react.png"
+              src="./images/typescript.png"
             />
-            
-
+          </Figure>
+          {/* <img
+            className="d-block w-100 moviePoster"
+            src="https://m.media-amazon.com/images/M/MV5BYjJiZjMzYzktNjU0NS00OTkxLWEwYzItYzdhYWJjN2QzMTRlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+            alt="First slide"
+          /> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <Figure className="tech-logos">
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
@@ -44,33 +58,34 @@ const Technologies = ()=>{
             />
 
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
+              width={150}
+              height={150}
+              alt="171x180"
+              src="./images/git.png"
+            />
+
+            <Figure.Image
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
               src="./images/postgresql.png"
             />
-            
-          </Figure>
-          {/* <img
-            className="d-block w-100 moviePoster"
-            src="https://m.media-amazon.com/images/M/MV5BYjJiZjMzYzktNjU0NS00OTkxLWEwYzItYzdhYWJjN2QzMTRlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-            alt="First slide"
-          /> */}
-        <Carousel.Caption>
-        </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <Figure className="tech-logos">
-          
-            
-            
-
-            
-            
 
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
+              width={100}
+              height={100}
+              alt="171x180"
+              src="./images/java.png"
+            />
+          </Figure>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Figure className="tech-logos">
+            <Figure.Image
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
@@ -78,64 +93,34 @@ const Technologies = ()=>{
             />
 
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
               src="./images/express.png"
             />
-            
+
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
               width={150}
               height={150}
               alt="171x180"
               src="./images/HTML5-logo.png"
             />
-            
 
             <Figure.Image
-            className="tech-icon"
+              className="tech-icon"
               width={100}
               height={100}
               alt="171x180"
               src="./images/css3.png"
             />
-           
           </Figure>
-            {/* <img
-              width={900}
-              height={500}
-              className="d-block w-100 moviePoster"
-              src="https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg"
-              alt="Second slide"
-            />
-            <img
-              width={900}
-              height={500}
-              className="d-block w-100 moviePoster"
-              src="https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg"
-              alt="Second slide"
-            /> */}
-            <Carousel.Caption>
-        </Carousel.Caption>
         </Carousel.Item>
-        {/* <Carousel.Item> */}
-        {/* <Figure className="test12"> */}
-            
-            
-          {/* </Figure> */}
-          {/* <img
-            className="d-block w-100 moviePoster"
-            src="https://m.media-amazon.com/images/M/MV5BMjA4NDI0MTIxNF5BMl5BanBnXkFtZTYwNTM0MzY2._V1_SX300.jpg"
-            alt="Third slide"
-          /> */}
-
-        {/* </Carousel.Item> */}
+        
       </Carousel>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default Technologies
-
+export default Technologies;
